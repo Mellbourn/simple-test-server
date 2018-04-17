6be3d96608f97411496daaff81503269265f38df
Klas Mellbourn <klas@mellbourn.net>
2018-04-17 17:34:17 +0200

    add eslinting

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 
 app.get("/*", (req, res) => {
-  res.status(202);
-  console.log("responding with 202", req);
-  res.send("responded with 202!");
-});
+  res.status(202)
+  console.log("responding with 202", req)
+  res.send()
+})
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(3000, () => console.log("Example app listening on port 3000!"))
